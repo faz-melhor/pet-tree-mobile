ea576c81c847022cf812e27c600c47e4432f248c
Marcos Insfran <rester.marcos@gmail.com>
2021-04-07 23:00:13 -0300

    Add register screen

--- screens/RegisterTreeScreen.js
+++ screens/RegisterTreeScreen.js
@@ -1,10 +1,86 @@
 import * as React from "react";
-import { Text, StyleSheet } from "react-native";
+import { StyleSheet, Image, Text, View } from "react-native";
+import { TextInput, Button } from "react-native-paper";
 
-const RegisterTreeScreen = () => {
-  return <Text>Screen Test</Text>;
+const RegisterScreen = () => {
+  const [email, setEmail] = React.useState("");
+  const [nome, setNome] = React.useState("");
+
+  const [password, setPassword] = React.useState("");
+  const [passwordConfirmation, setPasswordConfirmation] = React.useState("");
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.item}
+        label="E-mail"
+        mode="outlined"
+        value={email}
+        onChangeText={(email) => setEmail(email)}
+      />
+      <TextInput
+        style={styles.item}
+        label="Nome"
+        mode="outlined"
+        value={nome}
+        onChangeText={(nome) => setNome(nome)}
+      />
+      <TextInput
+        style={styles.item}
+        label="Senha"
+        mode="outlined"
+        secureTextEntry={true}
+        value={password}
+        onChangeText={(password) => setPassword(password)}
+      />
+      <TextInput
+        style={styles.item}
+        label="Confirmar Senha"
+        mode="outlined"
+        secureTextEntry={true}
+        value={passwordConfirmation}
+        onChangeText={(passwordConfirmation) =>
+          setPasswordConfirmation(passwordConfirmation)
+        }
+      />
+      <Button
+        style={styles.button}
+        contentStyle={{ height: 50 }}
+        mode="contained"
+        onPress={() => console.log("Pressed")}
+      >
+        Entrar
+      </Button>
+    </View>
+  );
 };
 
-const stylesheet = StyleSheet.create({});
+export default RegisterScreen;
 
-export default RegisterTreeScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  textContainer: {
+    margin: 10,
+    flex: 1,
+  },
+  item: {
+    marginVertical: 10,
+  },
+  button: {
+    marginVertical: 10,
+  },
+  innerText: {
+    color: "#43a047",
+  },
+  containerOptions: {
+    alignItems: "center",
+  },
+
+  optionsText: {
+    marginTop: 10,
+    fontSize: 16,
+    alignItems: "center",
+  },
+});
