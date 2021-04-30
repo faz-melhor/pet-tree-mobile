5c9af43246aa7f164791db0efd1de4d2435d0f6e
Marcos Insfran <rester.marcos@gmail.com>
2021-04-30 00:26:19 -0300

    Add map marker

--- screens/TreeMapScreen.js
+++ screens/TreeMapScreen.js
@@ -1,34 +1,29 @@
 import React, { useState, useEffect } from "react";
-import { Text, StyleSheet } from "react-native";
-import MapView, { Region } from "react-native-maps";
+import { View, Text, StyleSheet } from "react-native";
+import MapView, { Marker } from "react-native-maps";
 import * as Location from "expo-location";
-
-const initialRegion = {
-  latitude: -25.519028,
-  longitude: -54.574422,
-  latitudeDelta: 0.0922,
-  longitudeDelta: 0.0421,
-};
+import { TextInput, IconButton } from "react-native-paper";
 
 const TreeMapScreen = () => {
-  const [region, setRegion] = useState(initialRegion);
+  const [region, setRegion] = useState();
 
   const getCurrentPosition = async () => {
     let { status } = await Location.requestPermissionsAsync();
 
     if (status !== "granted") {
-      Alert.alert("Ops!", "Permissão de acesso a localização negada.");
+      Alert.alert("Ops!", "Permissão de acesso à localização negada.");
     }
 
     let {
       coords: { latitude, longitude },
     } = await Location.getCurrentPositionAsync();
-
+    console.log("lat:", latitude, "long: ", longitude);
     setRegion({
       latitude,
       longitude,
       latitudeDelta: 0.0922,
       longitudeDelta: 0.0421,
+      timing: 1000,
     });
   };
 
@@ -36,7 +31,35 @@ const TreeMapScreen = () => {
     getCurrentPosition();
   }, []);
 
-  return <MapView initialRegion={region} region={region} style={styles.map} />;
+  return (
+    <View style={{ flex: 1 }}>
+      <MapView region={region} style={styles.map}>
+        <Marker
+          coordinate={{ latitude: -25.519028, longitude: -54.574422 }}
+          image={require("../assets/tree_1.png")}
+        />
+        <Marker
+          coordinate={{ latitude: -25.3074137, longitude: -52.5423814 }}
+          image={require("../assets/tree_0.png")}
+        />
+      </MapView>
+      <View
+        style={{
+          position: "absolute",
+          bottom: "5%",
+          alignSelf: "center",
+        }}
+      >
+        <IconButton
+          style={{ backgroundColor: "#43a047" }}
+          color="white"
+          icon="plus"
+          size={50}
+          onPress={() => console.log("Plus Pressed")}
+        />
+      </View>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
