4297a9672c991ef9278be75d4ea89ae9d1207ff6
Marcos Insfran <rester.marcos@gmail.com>
2021-07-14 23:31:21 -0300

    Auth getting token

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,47 +1,69 @@
 import * as React from "react";
 import { StyleSheet, Image, Text, View } from "react-native";
-import { TextInput, Button } from "react-native-paper";
+import {
+  AppForm,
+  AppFormField,
+  SubmitButton,
+  ErrorMessage,
+} from "../components/forms";
+import authApi from "../api/auth";
+import * as Yup from "yup";
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string().required().email().label("Email"),
+  password: Yup.string().required().min(4).label("Password"),
+});
 
 const WelcomeScreen = () => {
-  const [text, setText] = React.useState("");
-  const [password, setPassword] = React.useState("");
+  const [loginFailed, setLoginFailed] = React.useState(false);
+
+  const handleSubmit = async ({ email, password }) => {
+    result = await authApi.login(email, password);
+    if (!result.ok) return setLoginFailed(true);
+    setLoginFailed(false);
+    console.log("Hey, it works:", result.data);
+  };
 
   return (
     <View style={styles.container}>
       <View style={styles.logoContainer}>
         <Image source={require("../assets/leaf_logo.png")} />
       </View>
-      <View style={styles.textContainer}>
-        <TextInput
-          style={styles.item}
-          label="E-mail"
-          mode="outlined"
-          value={text}
-          onChangeText={(text) => setText(text)}
+      <AppForm
+        initialValues={{ email: "", password: "" }}
+        onSubmit={handleSubmit}
+        validationSchema={validationSchema}
+      >
+        <ErrorMessage
+          error="Invalid email and/or password."
+          visible={loginFailed}
         />
-        <TextInput
-          style={styles.item}
-          label="Senha"
+        <AppFormField
+          autoCapitalize="none"
+          autoCorrect={false}
           mode="outlined"
-          secureTextEntry={true}
-          value={password}
-          onChangeText={(password) => setPassword(password)}
+          keyboardType="email-address"
+          name="email"
+          placeholder="Email"
+          textContentType="emailAddress"
         />
-        <Button
-          style={styles.button}
-          contentStyle={{ height: 50 }}
-          mode="contained"
-          onPress={() => console.log("Pressed")}
-        >
-          Entrar
-        </Button>
+        <AppFormField
+          autoCapitalize="none"
+          autoCorrect={false}
+          mode="outlined"
+          name="password"
+          placeholder="Password"
+          secureTextEntry
+          textContentType="password"
+        />
+        <SubmitButton title="Log In" />
         <View style={styles.containerOptions}>
           <Text style={styles.optionsText}>Esqueceu sua senha?</Text>
           <Text style={styles.optionsText}>
             ou <Text style={styles.innerText}>Crie sua conta</Text>
           </Text>
         </View>
-      </View>
+      </AppForm>
     </View>
   );
 };
@@ -51,21 +73,12 @@ export default WelcomeScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    margin: 10,
   },
   logoContainer: {
     justifyContent: "center",
     alignItems: "center",
     flex: 1,
-  },
-  textContainer: {
-    margin: 10,
-    flex: 1,
-  },
-  item: {
-    marginVertical: 10,
-  },
-  button: {
-    marginVertical: 10,
   },
   innerText: {
     color: "#43a047",
